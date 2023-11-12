df9a6fae8031291398efba0ebac731c73fa8652f
Kodaskora <tomas.kutniauskis@gmail.com>
2023-11-12 11:11:28 +0100

    Create new elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+//import React from 'react';
+import Costs from './components/Costs/Costs';
 
 function App() {
+  const costs = [
+    {
+      date: new Date(2021, 2, 12),
+      description: 'Saldytuvas',
+      amount: 999.99,
+    },
+    {
+      date: new Date(2021, 11, 25),
+      description: 'MacBook',
+      amount: 1245.71,
+    },
+    {
+      date: new Date(2021, 4, 1),
+      description: 'Jeens',
+      amount: 48.99,
+    },
+  ];
+
+  // return (
+  //   React.createElement('div', {}, React.createElement('h1', {}, 'Start React'), React.createElement(Costs, {costs}))
+  // );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Start React</h1>
+      <Costs costs={costs} />
     </div>
   );
 }
